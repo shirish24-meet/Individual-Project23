e16db6ec3c19ea59e43c837d1a1f1a6d3c8a87af
shirish24-meet <139742507+shirish24-meet@users.noreply.github.com>
2023-07-25 15:56:13 +0300

    still working

--- Individual-Project/app.py
+++ Individual-Project/app.py
@@ -6,9 +6,36 @@ app = Flask(__name__, template_folder='templates', static_folder='static')
 app.config['SECRET_KEY'] = 'super-secret-key'
 
 #Code goes below here
+@app.route('/signin', methods=['GET', 'POST'])
+def signin():
+	error = ""
+	if request.method == 'POST':
+		email = request.form['email']
+		password = request.form['password']
+		try:
+			login_session['user'] = auth.sign_in_with_email_and_password(email, password)
+			return redirect(url_for('add_tweet'))
+		except:
+			error = "Authentication failed"
+	return render_template("signup.html")
 
 
-
+@app.route('/', methods=['GET', 'POST'])
+def signup():
+	error = ""
+	if request.method == 'POST':
+		email = request.form['email']
+		password = request.form['password']
+		full_name = request.form['full_name']
+		username = request.form['username']
+		bio = request.form['bio']
+		try:
+			user = {"email" : email, "password" : password,"full_name" :full_name, "username" : username, "bio": bio}
+			login_session['user'] = auth.create_user_with_email_and_password(email, password)
+			return redirect(url_for('add_tweet'))
+		except:
+			error = "Authentication failed"
+	return render_template("signup.html")
 
 #Code goes above here
 
